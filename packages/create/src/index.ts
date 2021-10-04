9857c079638af601b89279d891f6a48e98c80165
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-10-04 14:53:37 +0200

    Pin holochain 108

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -3,7 +3,7 @@ import semver from 'semver';
 import chalk from 'chalk';
 import { launchApp } from './app';
 
-console.log('@holochain/create v0.0.14');
+console.log('@holochain/create v0.0.15');
 
 (async () => {
   try {
