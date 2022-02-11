5dd566841f1402dd429f2c469547fd5bb9487c89
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-02-11 12:53:38 +0100

    Fixed vite open

--- patcher/vue/src/app/viteConfigTs.ts
+++ patcher/vue/src/app/viteConfigTs.ts
@@ -2,13 +2,16 @@ import { PatcherFile, PatcherNodeType } from '@patcher/types';
 
 export const viteConfigTs = (): PatcherFile => ({
   type: PatcherNodeType.File,
-  content: `import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+  content: `import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    open: true,
+  },
+});
 `
 });
     
