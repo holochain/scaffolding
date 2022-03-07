a85af1b1bec571261eb218c014e09785e9a10c4f
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-03-07 20:46:09 +0100

    Fixed tests

--- packages/svelte/tests/index.ts
+++ packages/svelte/tests/index.ts
@@ -1,6 +1,6 @@
 import test from 'tape';
 import path from 'path';
-import { generateLitApp } from '../dist';
+import { generateSvelteApp } from '../dist';
 import { writeDirectoryTree } from '@source-craft/fs';
 
 import { fileURLToPath } from 'url';
@@ -12,11 +12,11 @@ import { webHapp } from '@holochain-scaffolding/generators';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('generate a lit webHapp', async t => {
+test('generate a svelte webHapp', async t => {
   const happDef = newHappDef('hello-world');
-  const litApp = generateLitApp(happDef);
+  const svelteApp = generateSvelteApp(happDef);
 
-  const generatedWebHapp = webHapp(happDef, litApp);
+  const generatedWebHapp = webHapp(happDef, svelteApp);
 
   writeDirectoryTree(`${__dirname}/.fixture`, generatedWebHapp);
 });
