01a76f9ead16ed3b7094e6371a8829b64e794777
Matthias <tuttiacc@hotmail.com>
2022-10-24 21:24:15 +0200

    added snake_case and no whitespace checks

--- crates/hc_scaffold/src/utils.rs
+++ crates/hc_scaffold/src/utils.rs
@@ -1,8 +1,9 @@
 use std::{ffi::OsString, path::PathBuf, vec};
 
-use dialoguer::{theme::ColorfulTheme, Select};
+use dialoguer::{theme::ColorfulTheme, Select, Input};
+use inflector::Inflector;
 
-use crate::error::ScaffoldResult;
+use crate::error::{ScaffoldResult, ScaffoldError};
 use crate::file_tree::FileTree;
 
 pub fn choose_directory_path(prompt: &String, app_file_tree: &FileTree) -> ScaffoldResult<PathBuf> {
@@ -64,5 +65,44 @@ fn get_folders_names(file_tree: &FileTree) -> Vec<String> {
             .map(|(n, _)| n.to_str().unwrap().to_string())
             .collect(),
         FileTree::File(_) => vec![],
+    }
+}
+
+
+pub fn input_snake_case(prompt: &String) -> anyhow::Result<String> {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+    .with_prompt(prompt)
+    .interact_text()?;
+
+    match input.is_snake_case() {
+        true => Ok(input),
+        false => input_snake_case(&format!("Input must be snake_case.\n{}", prompt)),
+    }
+}
+
+pub fn input_no_whitespace(prompt: &String) -> anyhow::Result<String> {
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+    .with_prompt(prompt)
+    .interact_text()?;
+
+    match input.as_str().contains(char::is_whitespace) {
+        false => input_snake_case(&format!("Input must *not* contain white spaces.\n{}", prompt)),
+        true => Ok(input),
+    }
+}
+
+/// Raises an error if input is not snake_case
+pub fn check_snake_case(input: String, identifier: &str) -> ScaffoldResult<String> {
+    match input.is_snake_case() {
+        true => Ok(input),
+        false => Err(ScaffoldError::InvalidStringFormat(format!("{} must be snake_case", identifier))),
+    }
+}
+
+/// Raises an error if input is contains white spaces
+pub fn check_no_whitespace(input: String, identifier: &str) -> ScaffoldResult<String> {
+    match input.as_str().contains(char::is_whitespace) {
+        true => Err(ScaffoldError::InvalidStringFormat(format!("{} must *not* contain whitespaces.", identifier))),
+        false => Ok(input),
     }
 }
