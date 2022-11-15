523cd64fb93552dc9260386c11633fa9b37189ab
Guillem Córdoba <guillem.cordoba@gmail.com>
2022-11-15 18:27:47 +0100

    CI

--- default.nix
+++ default.nix
@@ -2,6 +2,10 @@ let
   holonixPath = (import ./nix/sources.nix).holonix; # points to the current state of the Holochain repository
   holonix = import (holonixPath) {
      # holochainVersionId = "v0_0_124"; specifies the Holochain version
+    rustVersion = {
+      track = "stable";
+      version = "1.64.0";
+    };
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
