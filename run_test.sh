6f362aecd46e19c047ff50b51329629fab5d0e17
Guillem Córdoba <guillem.cordoba@gmail.com>
2022-11-24 20:05:06 +0100

    Svelte template

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/bash
 set -e
 
+rm -rf /tmp/forum-svelte
+cd /tmp
+
+hc-scaffold web-app forum-svelte --setup-nix true --template svelte
+cd forum-svelte
+hc-scaffold dna forum 
+hc-scaffold zome posts --integrity dnas/forum/zomes/integrity/ --coordinator dnas/forum/zomes/coordinator/
+hc-scaffold entry-type post --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,content:String:TextArea
+hc-scaffold entry-type comment --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
+hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like
+hc-scaffold entry-type certificate --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate
+
+hc-scaffold index global all_posts post 
+hc-scaffold index by-author posts_by_author post
+hc-scaffold index global all_posts_entry_hash post:EntryHash
+hc-scaffold index by-author posts_by_author_entry_hash post:EntryHash
+
+hc-scaffold link-type post like --bidireccional false
+hc-scaffold link-type comment like:EntryHash --bidireccional true
+hc-scaffold link-type certificate:EntryHash like --bidireccional false
+hc-scaffold link-type agent:creator post:EntryHash --bidireccional true
+
+nix-shell . --run "
+set -e
+npm i
+npm run build -w ui
+"
+
 rm -rf /tmp/forum-vue
 cd /tmp
 
