7eda4c5482dbcccd057164bc7669f21db89fd3c1
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-10 19:35:16 +0100

    Fixed comments

--- src/scaffold/zome/coordinator.rs
+++ src/scaffold/zome/coordinator.rs
@@ -60,19 +60,19 @@ pub fn initial_lib_rs(dependencies: &Option<Vec<String>>) -> String {
         r#"use hdk::prelude::*;
 {integrity_imports}
 
-/// Called the first time a zome call is made to the cell containing this zome
+// Called the first time a zome call is made to the cell containing this zome
 #[hdk_extern]
 pub fn init(_: ()) -> ExternResult<InitCallbackResult> {{
   Ok(InitCallbackResult::Pass)
 }}
 
-/// Don't modify this enum if you want the scaffolding tool to generate appropriate signals for your entries and links
+// Don't modify this enum if you want the scaffolding tool to generate appropriate signals for your entries and links
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Signal {{
 }}
 
-/// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
+// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
 #[hdk_extern(infallible)]
 pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {{
     /// Don't modify this loop if you want the scaffolding tool to generate appropriate signals for your entries and links
