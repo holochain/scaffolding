458462848a6b812a5a9294ebcf95af9beefe9dc8
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-10 13:29:12 +0100

    Added comments on signals

--- src/scaffold/zome/coordinator.rs
+++ src/scaffold/zome/coordinator.rs
@@ -66,6 +66,7 @@ pub fn init(_: ()) -> ExternResult<InitCallbackResult> {{
   Ok(InitCallbackResult::Pass)
 }}
 
+/// Don't modify this enum if you want the scaffolding tool to generate appropriate signals for your entries and links
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Signal {{
@@ -74,6 +75,7 @@ pub enum Signal {{
 /// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
 #[hdk_extern(infallible)]
 pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {{
+    /// Don't modify this loop if you want the scaffolding tool to generate appropriate signals for your entries and links
     for action in committed_actions {{
         if let Err(err) = signal_action(action) {{
             error!("Error signaling new action: {{:?}}", err);
@@ -81,6 +83,7 @@ pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {{
     }}
 }}
 
+/// Don't modify this function if you want the scaffolding tool to generate appropriate signals for your entries and links
 fn signal_action(action: SignedActionHashed) -> ExternResult<()> {{
     Ok(())
 }}
