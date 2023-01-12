c8af11149875151b7100b88d6a9cb68d8ebfcff2
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-12 15:35:43 +0100

    Upped holochain client

--- src/versions.rs
+++ src/versions.rs
@@ -3,7 +3,7 @@ pub fn tryorama_version() -> String {
 }
 
 pub fn holochain_client_version() -> String {
-    String::from("^0.11.9")
+    String::from("^0.11.10")
 }
 
 pub fn hdi_version() -> String {
