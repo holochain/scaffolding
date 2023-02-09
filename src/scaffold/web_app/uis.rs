07437c625c3b788d055aa0be336c29ba7b6a9ecf
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-09 16:33:26 +0100

    Removed vanilla

--- src/scaffold/web_app/uis.rs
+++ src/scaffold/web_app/uis.rs
@@ -78,7 +78,7 @@ pub fn guess_or_choose_framework(app_file_tree: &FileTree) -> ScaffoldResult<UiF
 }
 
 pub fn choose_ui_framework() -> ScaffoldResult<UiFramework> {
-    let frameworks = vec!["Vanilla", "Vue", "Svelte", "Lit"];
+    let frameworks = vec!["Vue", "Svelte", "Lit"];
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Choose UI framework:")
         .default(0)
