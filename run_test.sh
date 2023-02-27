01ad44a220dec758faaff2bdf6ffd9a2b63d7f7b
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-27 13:29:08 +0100

    Fixed various bugs

--- run_test.sh
+++ run_test.sh
@@ -13,17 +13,18 @@ hc-scaffold dna forum
 hc-scaffold zome posts --integrity dnas/forum/zomes/integrity/ --coordinator dnas/forum/zomes/coordinator/
 hc-scaffold entry-type post --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,content:String:TextArea
 hc-scaffold entry-type comment --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
-hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,string_list:Vec\<String\>
+hc-scaffold entry-type likes --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Likes,string_list:Vec\<String\>
 hc-scaffold entry-type certificate --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate,certificate_type:Enum::CertificateType:TypeOne.TypeTwo,dna_hash:DnaHash
 
 hc-scaffold collection global all_posts post 
 hc-scaffold collection by-author posts_by_author post
 hc-scaffold collection global all_posts_entry_hash post:EntryHash
+hc-scaffold collection global all_likes likes
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 
-hc-scaffold link-type post like --delete true --bidireccional false
-hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
+hc-scaffold link-type post likes --delete true --bidireccional false
+hc-scaffold link-type comment likes:EntryHash --delete true --bidireccional true
+hc-scaffold link-type certificate:EntryHash likes --delete false --bidireccional false
 hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
 
 
@@ -44,7 +45,7 @@ hc-scaffold dna forum
 hc-scaffold zome posts --integrity dnas/forum/zomes/integrity/ --coordinator dnas/forum/zomes/coordinator/
 hc-scaffold entry-type post --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,content:String:TextArea
 hc-scaffold entry-type comment --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
-hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,string_list:Vec\<String\>
+hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Likes,string_list:Vec\<String\>
 hc-scaffold entry-type certificate --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate,certificate_type:Enum::CertificateType:TypeOne.TypeTwo,dna_hash:DnaHash
 
 hc-scaffold collection global all_posts post 
@@ -74,7 +75,7 @@ hc-scaffold dna forum
 hc-scaffold zome posts --integrity dnas/forum/zomes/integrity/ --coordinator dnas/forum/zomes/coordinator/
 hc-scaffold entry-type post --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,content:String:TextArea
 hc-scaffold entry-type comment --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
-hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,string_list:Vec\<String\>
+hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Likes,string_list:Vec\<String\>
 hc-scaffold entry-type certificate --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate,certificate_type:Enum::CertificateType:TypeOne.TypeTwo,dna_hash:DnaHash
 
 hc-scaffold collection global all_posts post 
@@ -107,7 +108,7 @@ hc-scaffold dna forum
 hc-scaffold zome posts --integrity dnas/forum/zomes/integrity/ --coordinator dnas/forum/zomes/coordinator/
 hc-scaffold entry-type post --reference-entry-hash false --crud crud --link-from-original-to-each-update true --fields title:String:TextField,content:String:TextArea
 hc-scaffold entry-type comment --reference-entry-hash false --crud crud --link-from-original-to-each-update false --fields post_hash:ActionHash::Post
-hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Like,string_list:Vec\<String\>
+hc-scaffold entry-type like --reference-entry-hash false --crud crd --fields like_hash:Option\<ActionHash\>::Likes,string_list:Vec\<String\>
 hc-scaffold entry-type certificate --reference-entry-hash true --crud cr --fields post_hash:ActionHash::Post,agent:AgentPubKey::certified,certifications_hashes:Vec\<EntryHash\>::Certificate,certificate_type:Enum::CertificateType:TypeOne.TypeTwo,dna_hash:DnaHash
 
 hc-scaffold collection global all_posts post 
