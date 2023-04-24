1587f40fec396d50a46a5758722c2da788f399ef
= <=>
2023-04-24 15:59:30 -0600

    clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   inputs = {
     nixpkgs.follows = "holochain-flake/nixpkgs";
 
-    # this can now be udpated directly, e.g.:
-    # nix flake lock --override-input holochain github:holochain/holochain/holochain-0.1.3
     holochain.url = "github:holochain/holochain";
     holochain.flake = false;
 
     holochain-flake = {
       url = "github:holochain/holochain";
-      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
+      inputs.versions.url = "github:holochain/holochain?dir=versions/0_1";
       inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0-beta-rc.6";
     };
   };
@@ -32,8 +30,7 @@
             devShells.default = pkgs.mkShell {
               inputsFrom = [ inputs.holochain-flake.devShells.${system}.holonix ];
               packages = with pkgs; [
-                  # more packages go here
-                  pkgs.nodejs-18_x pkgs.binaryen 
+                  pkgs.nodejs-18_x 
               ];
             };
           };
