53cf5cd6e5317a8bf81ce18aca1e812d2524a97f
= <=>
2023-09-18 14:11:37 -0600

    Flake and workflow changes

--- flake.nix
+++ flake.nix
@@ -11,25 +11,32 @@ inputs = {
     };
   };
 
-  outputs = inputs @ { ... }:
-    inputs.holochain-flake.inputs.flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-      }
-      {
-        systems = builtins.attrNames inputs.holochain-flake.devShells;
-        perSystem =
-          { config
-          , pkgs
-          , system
-          , ...
-          }: {
-            devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs.holochain-flake.devShells.${system}.holonix ];
-              packages = with pkgs; [
-                  pkgs.nodejs-18_x 
-              ];
-            };
-          };
+  outputs = inputs @ {...}:
+    inputs.holochain.inputs.flake-parts.lib.mkFlake
+    {
+      inherit inputs;
+    }
+    {
+      systems = builtins.attrNames inputs.holochain.devShells;
+      perSystem = {
+        self',
+        inputs',
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [inputs'.holochain.devShells.rustDev];
+          packages = [pkgs.nodejs-18_x];
+        };
+
+        devShells.ci = pkgs.mkShell {
+          inputsFrom = [self'.devShells.default];
+          packages = [
+            inputs'.holochain.packages.hc-scaffold
+          ];
+        };
       };
+    };
 }
