e039be272de0dbe9ccbfc1bbe626fce59f902b67
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-01-08 16:54:59 +0300

    Remove explicit holochain-integrity-types dependency

--- src/scaffold/zome.rs
+++ src/scaffold/zome.rs
@@ -339,11 +339,6 @@ pub fn add_common_zome_dependencies_to_workspace_cargo(
         &"hdk".to_string(),
         &format!("={}", hdk_version()),
     )?;
-    let file_tree = add_workspace_external_dependency(
-        file_tree,
-        &"holochain_integrity_types".to_string(),
-        &"^0.1.2".to_string(),
-    )?;
     let file_tree =
         add_workspace_external_dependency(file_tree, &"serde".to_string(), &"=1.0.166".to_string())?;
 
