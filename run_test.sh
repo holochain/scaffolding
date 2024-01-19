765f6117cbe9cafd43fe04b7f67ad5d1fbcf1acf
Matt Gabrenya <matt@buildyourweb.app>
2024-01-19 12:53:21 -0700

    chore: fix spelling 'bidireccional' -> 'bidirectional'

--- run_test.sh
+++ run_test.sh
@@ -24,10 +24,10 @@ hc-scaffold collection global all_posts_entry_hash post:EntryHash
 hc-scaffold collection global all_likes like
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 
-hc-scaffold link-type post like --delete true --bidireccional false
-hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
-hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
+hc-scaffold link-type post like --delete true --bidirectional false
+hc-scaffold link-type comment like:EntryHash --delete true --bidirectional true
+hc-scaffold link-type certificate:EntryHash like --delete false --bidirectional false
+hc-scaffold link-type agent:creator post:EntryHash --delete false --bidirectional true
 
 
 nix develop --command bash -c "
@@ -57,10 +57,10 @@ hc-scaffold collection global all_likes like
 hc-scaffold collection global all_posts_entry_hash post:EntryHash
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 
-hc-scaffold link-type post like --delete true --bidireccional false
-hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
-hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
+hc-scaffold link-type post like --delete true --bidirectional false
+hc-scaffold link-type comment like:EntryHash --delete true --bidirectional true
+hc-scaffold link-type certificate:EntryHash like --delete false --bidirectional false
+hc-scaffold link-type agent:creator post:EntryHash --delete false --bidirectional true
 
 nix develop --command bash -c "
 set -e
@@ -89,10 +89,10 @@ hc-scaffold collection global all_posts_entry_hash post:EntryHash
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 hc-scaffold collection global all_likes like
 
-hc-scaffold link-type post like --delete true --bidireccional false
-hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
-hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
+hc-scaffold link-type post like --delete true --bidirectional false
+hc-scaffold link-type comment like:EntryHash --delete true --bidirectional true
+hc-scaffold link-type certificate:EntryHash like --delete false --bidirectional false
+hc-scaffold link-type agent:creator post:EntryHash --delete false --bidirectional true
 
 nix develop --command bash -c "
 set -e
@@ -124,10 +124,10 @@ hc-scaffold collection global all_posts_entry_hash post:EntryHash
 hc-scaffold collection global all_likes like
 hc-scaffold collection by-author posts_by_author_entry_hash post:EntryHash
 
-hc-scaffold link-type post like --delete true --bidireccional false
-hc-scaffold link-type comment like:EntryHash --delete true --bidireccional true
-hc-scaffold link-type certificate:EntryHash like --delete false --bidireccional false
-hc-scaffold link-type agent:creator post:EntryHash --delete false --bidireccional true
+hc-scaffold link-type post like --delete true --bidirectional false
+hc-scaffold link-type comment like:EntryHash --delete true --bidirectional true
+hc-scaffold link-type certificate:EntryHash like --delete false --bidirectional false
+hc-scaffold link-type agent:creator post:EntryHash --delete false --bidirectional true
 
 
 nix develop --command bash -c "
