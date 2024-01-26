67c3dfa7fb140435b96ab19936c73b3c0c115400
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-01-26 09:24:22 +0300

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,11 @@
 
   inputs = {
     nixpkgs.follows = "holochain/nixpkgs";
-    versions.url = "github:holochain/holochain?dir=versions/0_2";
+    versions.url = "github:holochain/holochain?dir=versions/weekly";
 
     holochain = {
       url = "github:holochain/holochain";
       inputs.versions.follows = "versions";
-      inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.33";
     };
   };
 
