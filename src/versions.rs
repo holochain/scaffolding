f7535a9c4cacb699b2ac1c5a415b450d226b036c
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-02-06 20:53:48 +0300

    chore: update holochain client version

--- src/versions.rs
+++ src/versions.rs
@@ -3,7 +3,7 @@ pub fn tryorama_version() -> String {
 }
 
 pub fn holochain_client_version() -> String {
-    String::from("^0.17.0-dev.3")
+    String::from("^0.17.0-dev.5")
 }
 
 // TODO: update to 0.3 compatible version
