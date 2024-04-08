a58132174ecd5341269635c0c7033c4cf214d7e8
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-04-08 18:22:02 +0300

    prefer eprintln

--- src/bin/hc-scaffold.rs
+++ src/bin/hc-scaffold.rs
@@ -5,7 +5,7 @@ use structopt::StructOpt;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     if let Err(e) = HcScaffold::from_args().run().await {
-        println!("{}", e.to_string().red());
+        eprintln!("{}", e.to_string().red());
     }
     Ok(())
 }
