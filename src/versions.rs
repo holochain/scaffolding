64e5dad28b2b623df04535e8c438a690765e3097
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-04-08 17:34:05 +0300

    chore: bump holochain client js version

--- src/versions.rs
+++ src/versions.rs
@@ -3,7 +3,7 @@ pub fn tryorama_version() -> String {
 }
 
 pub fn holochain_client_version() -> String {
-    String::from("^0.17.0-dev.7")
+    String::from("^0.17.0-dev.8")
 }
 
 pub fn hc_spin_version() -> String {
