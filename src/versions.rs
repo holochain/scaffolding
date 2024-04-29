0363425689e968574be710ca3adaa98d3f1bfbb5
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-04-29 15:01:27 +0300

    add doc comments to version consts

--- src/versions.rs
+++ src/versions.rs
@@ -1,10 +1,14 @@
+/// npm: https://www.npmjs.com/package/@holochain/tryorama
 pub const TRYORAMA_VERSION: &str = "^v0.16.0-dev.5";
 
+/// npm: https://www.npmjs.com/package/@holochain/client
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.17.0-dev.12";
 
+/// npm: https://www.npmjs.com/package/@holochain/hc-spin
 pub const HC_SPIN_VERSION: &str = "0.300.2-dev.5";
 
 // TODO: update to 0.3 compatible version
+/// npm: https://www.npmjs.com/package/@holo-host/web-sdk
 pub const WEB_SDK_VERSION: &str = "^0.6.10-prerelease";
 
 pub const HDI_VERSION: &str = holochain::HDI_VERSION;
@@ -13,7 +17,10 @@ pub const HDK_VERSION: &str = holochain::HDK_VERSION;
 
 pub const HOLOCHAIN_VERSION: &str = holochain::HOLOCHAIN_VERSION;
 
+/// https://github.com/holochain/holochain/tree/develop/versions
 pub const HOLOCHAIN_NIX_VERSION: &str = "weekly";
 
-/// https://github.com/darksoil-studio/holochain-playground
+/// source: https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server
+///
+/// npm: https://www.npmjs.com/package/@holochain-playground/cli
 pub const HOLOCHAIN_PLAYGROUND_CLI_VERSION: &str = "^0.2.0";
