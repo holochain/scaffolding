8d1aff930be056795ecf5b2a5078ecad5e370929
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-05-10 13:48:16 +0300

    improve readability of stringified rust code

--- src/scaffold/link_type/coordinator.rs
+++ src/scaffold/link_type/coordinator.rs
@@ -34,37 +34,40 @@ fn metadata_handlers(
     let pascal_link_type_name = link_type_name.to_case(Case::Pascal);
 
     format!(
-        r#"use hdk::prelude::*;
-use {integrity_zome_name}::*;
+        r#"
+        use hdk::prelude::*;
+        use {integrity_zome_name}::*;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Add{pascal_link_type_name}For{pascal_from}Input {{
-    pub {snake_from_arg}: {from_arg_type},
-    pub {snake_link_type_name}: String,
-}}
-#[hdk_extern]
-pub fn add_{snake_link_type_name}_for_{snake_from}(input: Add{pascal_link_type_name}For{pascal_from}Input) -> ExternResult<()> {{
-    create_link(input.{snake_from_arg}.clone(), input.{snake_from_arg}, LinkTypes::{pascal_link_type_name}, input.{snake_link_type_name})?;
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct Add{pascal_link_type_name}For{pascal_from}Input {{
+            pub {snake_from_arg}: {from_arg_type},
+            pub {snake_link_type_name}: String,
+        }}
 
-    Ok(())    
-}}
+        #[hdk_extern]
+        pub fn add_{snake_link_type_name}_for_{snake_from}(input: Add{pascal_link_type_name}For{pascal_from}Input) -> ExternResult<()> {{
+            create_link(input.{snake_from_arg}.clone(), input.{snake_from_arg}, LinkTypes::{pascal_link_type_name}, input.{snake_link_type_name})?;
 
-#[hdk_extern]
-pub fn get_{plural_snake_link_type_name}_for_{snake_from}({snake_from_arg}: {from_arg_type}) -> ExternResult<Vec<String>> {{
-    let links = get_links(
-        GetLinksInputBuilder::try_new({snake_from_arg}, LinkTypes::{pascal_link_type_name})?.build(),
-    )?;
-    
-    let {snake_link_type_name}: Vec<String> = links
-        .into_iter()
-        .map(|link| 
-          String::from_utf8(link.tag.into_inner())
-            .map_err(|e| wasm_error!(WasmErrorInner::Guest(format!("Error converting link tag to string: {{:?}}", e))))
-        )
-        .collect::<ExternResult<Vec<String>>>()?;
+            Ok(())    
+        }}
 
-    Ok({snake_link_type_name})
-}}"#
+        #[hdk_extern]
+        pub fn get_{plural_snake_link_type_name}_for_{snake_from}({snake_from_arg}: {from_arg_type}) -> ExternResult<Vec<String>> {{
+            let links = get_links(
+                GetLinksInputBuilder::try_new({snake_from_arg}, LinkTypes::{pascal_link_type_name})?.build(),
+            )?;
+            
+            let {snake_link_type_name}: Vec<String> = links
+                .into_iter()
+                .map(|link| 
+                String::from_utf8(link.tag.into_inner())
+                    .map_err(|e| wasm_error!(WasmErrorInner::Guest(format!("Error converting link tag to string: {{:?}}", e))))
+                )
+                .collect::<ExternResult<Vec<String>>>()?;
+
+            Ok({snake_link_type_name})
+        }}
+        "#
     )
 }
 
@@ -101,18 +104,21 @@ pub fn add_link_handler(
     };
 
     format!(
-        r#"#[derive(Serialize, Deserialize, Debug)]
-pub struct Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
-    pub base_{from_arg_name}: {from_hash_type},
-    pub target_{to_arg_name}: {to_hash_type},
-}}
-#[hdk_extern]
-pub fn add_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input) -> ExternResult<()> {{
-    create_link(input.base_{from_arg_name}.clone(), input.target_{to_arg_name}.clone(), LinkTypes::{normal_link_type_name}, ())?;
-    {bidirectional_create}
+        r#"
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
+            pub base_{from_arg_name}: {from_hash_type},
+            pub target_{to_arg_name}: {to_hash_type},
+        }}
 
-    Ok(())    
-}}"#
+        #[hdk_extern]
+        pub fn add_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input) -> ExternResult<()> {{
+            create_link(input.base_{from_arg_name}.clone(), input.target_{to_arg_name}.clone(), LinkTypes::{normal_link_type_name}, ())?;
+            {bidirectional_create}
+
+            Ok(())    
+        }}
+        "#
     )
 }
 
@@ -148,36 +154,38 @@ fn get_links_handler_to_agent(
     let get_deleted_links_handler = if delete {
         format!(
             r#"
-#[hdk_extern]
-pub fn get_deleted_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}(
-    {from_arg_name}: {from_hash_type},
-) -> ExternResult<Vec<(SignedActionHashed, Vec<SignedActionHashed>)>> {{
-    let details = get_link_details(
-        {from_arg_name},
-        LinkTypes::{pascal_link_type_name},
-        None,
-        GetOptions::default(),
-    )?;
-    Ok(details
-        .into_inner()
-        .into_iter()
-        .filter(|(_link, deletes)| !deletes.is_empty())
-        .collect())
-}}"#
+            #[hdk_extern]
+            pub fn get_deleted_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}(
+                {from_arg_name}: {from_hash_type},
+            ) -> ExternResult<Vec<(SignedActionHashed, Vec<SignedActionHashed>)>> {{
+                let details = get_link_details(
+                    {from_arg_name},
+                    LinkTypes::{pascal_link_type_name},
+                    None,
+                    GetOptions::default(),
+                )?;
+                Ok(details
+                    .into_inner()
+                    .into_iter()
+                    .filter(|(_link, deletes)| !deletes.is_empty())
+                    .collect())
+            }}
+            "#
         )
     } else {
         String::new()
     };
 
     format!(
-        r#"#[hdk_extern]
-pub fn get_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}({from_arg_name}: {from_hash_type}) -> ExternResult<Vec<Link>> {{
-    get_links(
-        GetLinksInputBuilder::try_new({from_arg_name}, LinkTypes::{pascal_link_type_name})?.build(),
-    )
-}}
-{get_deleted_links_handler}
-"#,
+        r#"
+        #[hdk_extern]
+        pub fn get_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}({from_arg_name}: {from_hash_type}) -> ExternResult<Vec<Link>> {{
+            get_links(
+                GetLinksInputBuilder::try_new({from_arg_name}, LinkTypes::{pascal_link_type_name})?.build(),
+            )
+        }}
+        {get_deleted_links_handler}
+        "#,
     )
 }
 
@@ -203,35 +211,37 @@ fn get_links_handler_to_entry(
     let get_deleted_links_handler = match delete {
         true => format!(
             r#"
-#[hdk_extern]
-pub fn get_deleted_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}(
-    {from_arg_name}: {from_hash_type},
-) -> ExternResult<Vec<(SignedActionHashed, Vec<SignedActionHashed>)>> {{
-    let details = get_link_details(
-        {from_arg_name},
-        LinkTypes::{pascal_link_type_name},
-        None,
-        GetOptions::default(),
-    )?;
-    Ok(details
-        .into_inner()
-        .into_iter()
-        .filter(|(_link, deletes)| !deletes.is_empty())
-        .collect())
-}}"#
+            #[hdk_extern]
+            pub fn get_deleted_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}(
+                {from_arg_name}: {from_hash_type},
+            ) -> ExternResult<Vec<(SignedActionHashed, Vec<SignedActionHashed>)>> {{
+                let details = get_link_details(
+                    {from_arg_name},
+                    LinkTypes::{pascal_link_type_name},
+                    None,
+                    GetOptions::default(),
+                )?;
+                Ok(details
+                    .into_inner()
+                    .into_iter()
+                    .filter(|(_link, deletes)| !deletes.is_empty())
+                    .collect())
+            }}
+            "#
         ),
         false => String::new(),
     };
 
     format!(
-        r#"#[hdk_extern]
-pub fn get_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}({from_arg_name}: {from_hash_type}) -> ExternResult<Vec<Link>> {{
-    get_links(
-        GetLinksInputBuilder::try_new({from_arg_name}, LinkTypes::{pascal_link_type_name})?.build(),
-    )
-}}
-{get_deleted_links_handler}
-"#,
+        r#"
+        #[hdk_extern]
+        pub fn get_{plural_snake_to_entry_type}_for_{singular_snake_from_entry_type}({from_arg_name}: {from_hash_type}) -> ExternResult<Vec<Link>> {{
+            get_links(
+                GetLinksInputBuilder::try_new({from_arg_name}, LinkTypes::{pascal_link_type_name})?.build(),
+            )
+        }}
+        {get_deleted_links_handler}
+        "#,
     )
 }
 
@@ -277,41 +287,44 @@ fn remove_link_handlers(
     let bidirectional_remove = match bidirectional {
         true => format!(
             r#"
-    let links = get_links(
-        GetLinksInputBuilder::try_new(input.target_{to_arg_name}.clone(), LinkTypes::{inverse_link_type_name})?.build(),
-    )?;
+            let links = get_links(
+                GetLinksInputBuilder::try_new(input.target_{to_arg_name}.clone(), LinkTypes::{inverse_link_type_name})?.build(),
+            )?;
 
-    for link in links {{
-        if {from_inverse}.eq(&input.base_{from_arg_name}) {{
-            delete_link(link.create_link_hash)?;
-        }}
-    }}"#
+            for link in links {{
+                if {from_inverse}.eq(&input.base_{from_arg_name}) {{
+                    delete_link(link.create_link_hash)?;
+                }}
+            }}
+            "#
         ),
         false => String::new(),
     };
 
     format!(
-        r#"#[derive(Serialize, Deserialize, Debug)]
-pub struct Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
-    pub base_{from_arg_name}: {from_hash_type},
-    pub target_{to_arg_name}: {to_hash_type},
-}}
-#[hdk_extern]
-pub fn remove_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input ) -> ExternResult<()> {{
-    let links = get_links(
-        GetLinksInputBuilder::try_new(input.base_{from_arg_name}.clone(), LinkTypes::{pascal_link_type_name})?.build(),
-    )?;
-    
-    for link in links {{
-        if {from_link}.eq(&input.target_{to_arg_name}) {{
-            delete_link(link.create_link_hash)?;
+        r#"
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
+            pub base_{from_arg_name}: {from_hash_type},
+            pub target_{to_arg_name}: {to_hash_type},
         }}
-    }}
-    {bidirectional_remove}
 
-    Ok(())        
-}}
-"#
+        #[hdk_extern]
+        pub fn remove_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input ) -> ExternResult<()> {{
+            let links = get_links(
+                GetLinksInputBuilder::try_new(input.base_{from_arg_name}.clone(), LinkTypes::{pascal_link_type_name})?.build(),
+            )?;
+            
+            for link in links {{
+                if {from_link}.eq(&input.target_{to_arg_name}) {{
+                    delete_link(link.create_link_hash)?;
+                }}
+            }}
+            {bidirectional_remove}
+
+            Ok(())        
+        }}
+        "#
     )
 }
 
@@ -324,9 +337,7 @@ fn normal_handlers(
 ) -> String {
     let inverse_get = match bidirectional {
         true => format!(
-            r#"
-
-{}"#,
+            "{}",
             get_links_handler(to_referenceable, from_referenceable, delete)
         ),
         false => String::new(),
@@ -337,20 +348,21 @@ fn normal_handlers(
         false => String::new(),
     };
 
+    let add_links_handler = add_link_handler(from_referenceable, to_referenceable, bidirectional);
+    let get_links_handler = get_links_handler(from_referenceable, to_referenceable, delete);
+
     format!(
-        r#"use hdk::prelude::*;
-use {integrity_zome_name}::*;
+        r#"
+        use hdk::prelude::*;
+        use {integrity_zome_name}::*;
 
-{}
+        {add_links_handler}
 
-{}
-{}
+        {get_links_handler}
+        {inverse_get}
         
-{}"#,
-        add_link_handler(from_referenceable, to_referenceable, bidirectional),
-        get_links_handler(from_referenceable, to_referenceable, delete),
-        inverse_get,
-        delete_link_handler
+        {delete_link_handler}
+        "#,
     )
 }
 
@@ -396,12 +408,13 @@ pub fn add_link_type_functions_to_coordinator(
 
     // 2. Add this file as a module in the entry point for the crate
 
-    map_file(&mut file_tree, &lib_rs_path, |file| {
+    map_file(&mut file_tree, &lib_rs_path, |contents| {
         Ok(format!(
-            r#"pub mod {};
+            r#"
+            pub mod {snake_link_type_name};
 
-{}"#,
-            snake_link_type_name, file
+            {contents}
+            "#,
         ))
     })?;
 
