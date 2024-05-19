2a273241b76b6d986ce176bd7b270fa40ddf65b7
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-05-19 21:37:54 +0300

    refactor link-type coordinator

--- src/scaffold/link_type/coordinator.rs
+++ src/scaffold/link_type/coordinator.rs
@@ -1,6 +1,6 @@
 use convert_case::{Case, Casing};
 use proc_macro2::TokenStream;
-use quote::format_ident;
+use quote::{format_ident, quote};
 
 use crate::{
     error::ScaffoldResult,
@@ -10,6 +10,7 @@ use crate::{
         entry_type::definitions::{Cardinality, EntryTypeReference, Referenceable},
         zome::ZomeFileTree,
     },
+    utils::unparse,
 };
 
 use super::link_type_name;
@@ -18,107 +19,147 @@ fn metadata_handlers(
     integrity_zome_name: &str,
     link_type_name: &str,
     from_referenceable: &Referenceable,
-) -> String {
-    let snake_from_arg = from_referenceable
-        .field_name(&Cardinality::Single)
-        .to_case(Case::Snake);
-    let from_arg_type = from_referenceable.hash_type().to_string();
+) -> TokenStream {
+    let snake_from_arg = format_ident!(
+        "{}",
+        from_referenceable
+            .field_name(&Cardinality::Single)
+            .to_case(Case::Snake)
+    );
+    let from_arg_type = format_ident!("{}", from_referenceable.hash_type().to_string());
+    let snake_from = format_ident!(
+        "{}",
+        from_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Snake)
+    );
+    let pascal_from = format_ident!(
+        "{}",
+        from_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Pascal)
+    );
+    let pascal_link_type_name = format_ident!("{}", link_type_name.to_case(Case::Pascal));
+    let snake_link_type_name = format_ident!("{}", link_type_name.to_case(Case::Snake));
+    let add_link_type_struct_name =
+        format_ident!("Add{pascal_link_type_name}For{pascal_from}Input");
+    let add_link_type_function_name = format_ident!("add_{snake_link_type_name}_for_{snake_from}");
+    let plural_snake_link_type_name = format_ident!(
+        "{}",
+        pluralizer::pluralize(&link_type_name.to_case(Case::Snake), 2, false)
+    );
+    let get_link_type_function_name =
+        format_ident!("get_{plural_snake_link_type_name}_for_{snake_from}");
 
-    let snake_from = from_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Snake);
-    let pascal_from = from_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Pascal);
-    let snake_link_type_name = link_type_name.to_case(Case::Snake);
-    let plural_snake_link_type_name =
-        pluralizer::pluralize(&link_type_name.to_case(Case::Snake), 2, false);
-    let pascal_link_type_name = link_type_name.to_case(Case::Pascal);
-
-    format!(
-        r#"
+    quote! {
         use hdk::prelude::*;
-        use {integrity_zome_name}::*;
+        use #integrity_zome_name::*;
 
         #[derive(Serialize, Deserialize, Debug)]
-        pub struct Add{pascal_link_type_name}For{pascal_from}Input {{
-            pub {snake_from_arg}: {from_arg_type},
-            pub {snake_link_type_name}: String,
-        }}
+        pub struct #add_link_type_struct_name {
+            pub #snake_from_arg: #from_arg_type,
+            pub #snake_link_type_name: String,
+        }
 
         #[hdk_extern]
-        pub fn add_{snake_link_type_name}_for_{snake_from}(input: Add{pascal_link_type_name}For{pascal_from}Input) -> ExternResult<()> {{
-            create_link(input.{snake_from_arg}.clone(), input.{snake_from_arg}, LinkTypes::{pascal_link_type_name}, input.{snake_link_type_name})?;
-
-            Ok(())    
-        }}
-
-        #[hdk_extern]
-        pub fn get_{plural_snake_link_type_name}_for_{snake_from}({snake_from_arg}: {from_arg_type}) -> ExternResult<Vec<String>> {{
-            let links = get_links(
-                GetLinksInputBuilder::try_new({snake_from_arg}, LinkTypes::{pascal_link_type_name})?.build(),
+        pub fn #add_link_type_function_name(input: #add_link_type_struct_name) -> ExternResult<()> {
+            create_link(
+                input.#snake_from_arg.clone(),
+                input.#snake_from_arg,
+                LinkTypes::#pascal_link_type_name,
+                input.#snake_link_type_name,
             )?;
-            
-            let {snake_link_type_name}: Vec<String> = links
+            Ok(())
+        }
+
+        #[hdk_extern]
+        pub fn #get_link_type_function_name(#snake_from_arg: #from_arg_type) -> ExternResult<Vec<String>> {
+            let links = get_links(
+                GetLinksInputBuilder::try_new(#snake_from_arg, LinkTypes::#pascal_link_type_name)?.build(),
+            )?;
+
+            let #snake_link_type_name: Vec<String> = links
                 .into_iter()
-                .map(|link| 
-                String::from_utf8(link.tag.into_inner())
-                    .map_err(|e| wasm_error!(WasmErrorInner::Guest(format!("Error converting link tag to string: {{:?}}", e))))
+                .map(|link|
+                    String::from_utf8(link.tag.into_inner())
+                        .map_err(|e| wasm_error!(WasmErrorInner::Guest(format!("Error converting link tag to string: {{:?}}", e))))
                 )
                 .collect::<ExternResult<Vec<String>>>()?;
 
-            Ok({snake_link_type_name})
-        }}
-        "#
-    )
+            Ok(#snake_link_type_name)
+        }
+    }
 }
 
 pub fn add_link_handler(
     from_referenceable: &Referenceable,
     to_referenceable: &Referenceable,
     bidirectional: bool,
-) -> String {
-    let from_hash_type = from_referenceable.hash_type().to_string();
-    let from_arg_name = from_referenceable.field_name(&Cardinality::Single);
-    let to_hash_type = to_referenceable.hash_type().to_string();
-    let to_arg_name = to_referenceable.field_name(&Cardinality::Single);
+) -> TokenStream {
+    let from_hash_type = format_ident!("{}", from_referenceable.hash_type().to_string());
+    let to_hash_type = format_ident!("{}", to_referenceable.hash_type().to_string());
+    let target_field_name = format_ident!(
+        "target_{}",
+        to_referenceable.field_name(&Cardinality::Single)
+    );
+    let base_field_name = format_ident!(
+        "base_{}",
+        from_referenceable.field_name(&Cardinality::Single)
+    );
+    let add_link_input_struct_name = format_ident!(
+        "Add{}For{}Input",
+        to_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Pascal),
+        from_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Pascal)
+    );
+    let add_link_function_name = format_ident!(
+        "add_{}_for_{}",
+        to_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Snake),
+        from_referenceable
+            .to_string(&Cardinality::Single)
+            .to_case(Case::Snake)
+    );
 
-    let normal_link_type_name = link_type_name(from_referenceable, to_referenceable);
-    let inverse_link_type_name = link_type_name(to_referenceable, from_referenceable);
-    let singular_snake_from_entry_type = from_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Snake);
-    let singular_pascal_from_entry_type = from_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Pascal);
-    let singular_snake_to_entry_type = to_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Snake);
-    let singular_pascal_to_entry_type = to_referenceable
-        .to_string(&Cardinality::Single)
-        .to_case(Case::Pascal);
+    let normal_link_type_name =
+        format_ident!("{}", link_type_name(from_referenceable, to_referenceable));
+    let inverse_link_type_name =
+        format_ident!("{}", link_type_name(to_referenceable, from_referenceable));
 
-    let bidirectional_create = bidirectional.then_some(
-        format!("create_link(input.target_{to_arg_name}, input.base_{from_arg_name}, LinkTypes::{inverse_link_type_name}, ())?;")
-    ).unwrap_or_default();
+    let bidirectional_create = bidirectional
+        .then_some(quote! {
+            create_link(
+                input.#target_field_name,
+                input.#base_field_name,
+                LinkTypes::#inverse_link_type_name,
+                (),
+            )?;
+        })
+        .unwrap_or_default();
 
-    format!(
-        r#"
+    quote! {
         #[derive(Serialize, Deserialize, Debug)]
-        pub struct Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
-            pub base_{from_arg_name}: {from_hash_type},
-            pub target_{to_arg_name}: {to_hash_type},
-        }}
+        pub struct #add_link_input_struct_name {
+            pub #base_field_name: #from_hash_type,
+            pub #target_field_name: #to_hash_type,
+        }
 
         #[hdk_extern]
-        pub fn add_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Add{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input) -> ExternResult<()> {{
-            create_link(input.base_{from_arg_name}.clone(), input.target_{to_arg_name}.clone(), LinkTypes::{normal_link_type_name}, ())?;
-            {bidirectional_create}
-
-            Ok(())    
-        }}
-        "#
-    )
+        pub fn #add_link_function_name(input: #add_link_input_struct_name) -> ExternResult<()> {
+            create_link(
+                input.#base_field_name.clone(),
+                input.#target_field_name.clone(),
+                LinkTypes::#normal_link_type_name,
+                (),
+            )?;
+            #bidirectional_create
+            Ok(())
+        }
+    }
 }
 
 pub fn get_links_handler(
@@ -265,29 +306,42 @@ fn get_links_handler_to_entry(
     }
 }
 
-fn from_link_hash_type(hash_type: &str) -> String {
-    let snake_hash_type = hash_type.to_case(Case::Snake);
-    let lower_hash_type = hash_type.to_case(Case::Lower);
-
+fn from_link_hash_type(hash_type: &str) -> TokenStream {
     match hash_type {
-        "AgentPubKey" => "AgentPubKey::from(link.target.clone().into_entry_hash().ok_or(wasm_error!(WasmErrorInner::Guest(\"No entry_hash associated with link\".to_string())))?)".to_string(),
-        _ => format!("link.target.clone().into_{}().ok_or(wasm_error!(WasmErrorInner::Guest(\"No {} associated with link\".to_string())))?", snake_hash_type, lower_hash_type),
+        "AgentPubKey" => quote! {
+            AgentPubKey::from(
+                link.target.clone()
+                    .into_entry_hash()
+                    .ok_or(wasm_error!(
+                        WasmErrorInner::Guest("No entry_hash associated with link".to_string())
+                    ))?
+            )
+        },
+        _ => {
+            let lower_hash_type = hash_type.to_case(Case::Lower);
+            let into_hash_method_name = format_ident!("into_{}", hash_type.to_case(Case::Snake));
+            quote! {
+                link.target
+                    .clone()
+                    .#into_hash_method_name()
+                    .ok_or(wasm_error!(
+                        WasmErrorInner::Guest(format!("No {} associated with link", #lower_hash_type))
+                    ))?
+            }
+        }
     }
 }
 
-// Event to calendar
 fn remove_link_handlers(
     from_referenceable: &Referenceable,
     to_referenceable: &Referenceable,
     bidirectional: bool,
-) -> String {
+) -> TokenStream {
     let from_hash_type = from_referenceable.hash_type().to_string();
     let from_arg_name = from_referenceable.field_name(&Cardinality::Single);
-    let to_hash_type = to_referenceable.hash_type().to_string();
-    let to_arg_name = to_referenceable.field_name(&Cardinality::Single);
 
-    let pascal_link_type_name = link_type_name(from_referenceable, to_referenceable);
-    let inverse_link_type_name = link_type_name(to_referenceable, from_referenceable);
+    let inverse_link_type_name =
+        format_ident!("{}", link_type_name(to_referenceable, from_referenceable));
     let singular_pascal_to_entry_type = to_referenceable
         .to_string(&Cardinality::Single)
         .to_case(Case::Pascal);
@@ -301,50 +355,58 @@ fn remove_link_handlers(
         .to_string(&Cardinality::Single)
         .to_case(Case::Snake);
 
-    let from_link = from_link_hash_type(&to_hash_type);
-    let from_inverse = from_link_hash_type(&from_hash_type);
+    let to_arg_name = format_ident!("{}", to_referenceable.field_name(&Cardinality::Single));
+
+    let remove_entry_for_entry_struct_name = format_ident!(
+        "Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input"
+    );
+    let base_field_name = format_ident!("base_{from_arg_name}");
+    let from_hash_type = format_ident!("{from_hash_type}");
+    let target_field_name = format_ident!("target_{to_arg_name}");
+    let to_hash_type = format_ident!("{}", to_referenceable.hash_type().to_string());
+
+    let remove_entry_for_entry_function_name =
+        format_ident!("remove_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}");
+    let pascal_link_type_name =
+        format_ident!("{}", link_type_name(from_referenceable, to_referenceable));
+
+    let from_link = from_link_hash_type(&to_hash_type.to_string());
+    let from_inverse = from_link_hash_type(&from_hash_type.to_string());
 
     let bidirectional_remove = bidirectional.then_some(
-        format!(
-            r#"
+        quote! {
             let links = get_links(
-                GetLinksInputBuilder::try_new(input.target_{to_arg_name}.clone(), LinkTypes::{inverse_link_type_name})?.build(),
+                GetLinksInputBuilder::try_new(input.#target_field_name.clone(), LinkTypes::#inverse_link_type_name)?.build(),
             )?;
-
             for link in links {{
-                if {from_inverse}.eq(&input.base_{from_arg_name}) {{
+                if #from_inverse == &input.#base_field_name {{
                     delete_link(link.create_link_hash)?;
                 }}
             }}
-            "#
-        )
+        }
     ).unwrap_or_default();
 
-    format!(
-        r#"
+    quote! {
         #[derive(Serialize, Deserialize, Debug)]
-        pub struct Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input {{
-            pub base_{from_arg_name}: {from_hash_type},
-            pub target_{to_arg_name}: {to_hash_type},
-        }}
+        pub struct #remove_entry_for_entry_struct_name {
+            pub #base_field_name: #from_hash_type,
+            pub #target_field_name: #to_hash_type,
+        }
 
         #[hdk_extern]
-        pub fn remove_{singular_snake_to_entry_type}_for_{singular_snake_from_entry_type}(input: Remove{singular_pascal_to_entry_type}For{singular_pascal_from_entry_type}Input ) -> ExternResult<()> {{
+        pub fn #remove_entry_for_entry_function_name(input: #remove_entry_for_entry_struct_name) -> ExternResult<()> {{
             let links = get_links(
-                GetLinksInputBuilder::try_new(input.base_{from_arg_name}.clone(), LinkTypes::{pascal_link_type_name})?.build(),
+                GetLinksInputBuilder::try_new(input.#base_field_name.clone(), LinkTypes::#pascal_link_type_name)?.build(),
             )?;
-            
             for link in links {{
-                if {from_link}.eq(&input.target_{to_arg_name}) {{
+                if #from_link == &input.#target_field_name {{
                     delete_link(link.create_link_hash)?;
                 }}
             }}
-            {bidirectional_remove}
-
-            Ok(())        
+            #bidirectional_remove
+            Ok(())
         }}
-        "#
-    )
+    }
 }
 
 fn normal_handlers(
@@ -353,7 +415,7 @@ fn normal_handlers(
     to_referenceable: &Referenceable,
     delete: bool,
     bidirectional: bool,
-) -> String {
+) -> TokenStream {
     let inverse_get = bidirectional
         .then_some(get_links_handler(
             to_referenceable,
@@ -370,22 +432,22 @@ fn normal_handlers(
         ))
         .unwrap_or_default();
 
+    let integrity_zome_name = format_ident!("{integrity_zome_name}");
     let add_links_handler = add_link_handler(from_referenceable, to_referenceable, bidirectional);
     let get_links_handler = get_links_handler(from_referenceable, to_referenceable, delete);
 
-    format!(
-        r#"
+    quote! {
         use hdk::prelude::*;
-        use {integrity_zome_name}::*;
+        use #integrity_zome_name::*;
 
-        {add_links_handler}
+        #add_links_handler
 
-        {get_links_handler}
-        {inverse_get}
-        
-        {delete_link_handler}
-        "#,
-    )
+        #get_links_handler
+
+        #inverse_get
+
+        #delete_link_handler
+    }
 }
 
 pub fn add_link_type_functions_to_coordinator(
@@ -408,11 +470,10 @@ pub fn add_link_type_functions_to_coordinator(
     let new_file_path = coordinator_zome_file_tree
         .zome_crate_path
         .join("src")
-        .join(format!("{}.rs", snake_link_type_name.clone()));
+        .join(format!("{}.rs", &snake_link_type_name));
     let crate_src_path = coordinator_zome_file_tree.zome_crate_path.join("src");
 
     let lib_rs_path = crate_src_path.join("lib.rs");
-
     let mut file_tree = coordinator_zome_file_tree.dna_file_tree.file_tree();
 
     let handlers = match to_referenceable {
@@ -425,23 +486,22 @@ pub fn add_link_type_functions_to_coordinator(
             bidirectional,
         ),
     };
+    let file = unparse(&syn::parse_quote! {
+        #handlers
+    });
 
-    insert_file(&mut file_tree, &new_file_path, &handlers)?;
+    insert_file(&mut file_tree, &new_file_path, &file)?;
 
     // 2. Add this file as a module in the entry point for the crate
-
     map_file(&mut file_tree, &lib_rs_path, |contents| {
         Ok(format!(
-            r#"
-            pub mod {snake_link_type_name};
+            r#"pub mod {snake_link_type_name};
 
-            {contents}
-            "#,
+{contents}"#,
         ))
     })?;
 
     let dna_file_tree = DnaFileTree::from_dna_manifest_path(file_tree, &dna_manifest_path)?;
-
     let zome_file_tree = ZomeFileTree::from_zome_manifest(dna_file_tree, zome_manifest)?;
 
     Ok(zome_file_tree)
