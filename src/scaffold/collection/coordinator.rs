1ffc9eb350b82784a8fb609073f1c7eccf2e0fc0
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-06-06 11:10:56 +0300

    chore: backport and bump scaffodling `0.3000.0 dev.13` (#306)
    * fix: poorly formatted rust code (#288)
    
    * fix: poorly formatted code in coordinator zome
    
    * document add_newlines
    
    * clearn up coordinators module
    
    * clean up integrity module
    
    * refactor utils
    
    * fix add_newlines
    
    * update integrity comments
    
    * fix map file calls
    
    * fix add_newlines
    
    * make add_newlines public/private function agnostic
    
    * refactor link-type coordinator
    
    * fix  remove link handler
    
    * fix collection coordinator
    
    * clean up entry-type coordinator
    
    * fix clippy warnings
    
    * refactor link-type coordinator
    
    * fix bug scaffolding entry-type with linked_from
    
    * fix target_hash_variable
    
    * remove unnecessary format usage
    
    * remove unnecessary clone
    
    * fix validate_delete_result
    
    * move comment
    
    * fix entry from rcord error message
    
    * rename unparse function to unparse_pretty;
    
    * feat: add long disable fast track option to hc scaffold web-app (#293)
    
    * add fix to prevent accumulation of dist.zip content (#300)
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: matthme <36768177+matthme@users.noreply.github.com>

--- src/scaffold/collection/coordinator.rs
+++ src/scaffold/collection/coordinator.rs
@@ -3,6 +3,9 @@ use std::ffi::OsString;
 use convert_case::{Case, Casing};
 use dialoguer::{theme::ColorfulTheme, Select};
 use holochain_types::prelude::ZomeManifest;
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use syn::parse_quote;
 
 use crate::{
     error::{ScaffoldError, ScaffoldResult},
@@ -16,6 +19,7 @@ use crate::{
             ZomeFileTree,
         },
     },
+    utils::unparse_pretty,
 };
 
 use super::CollectionType;
@@ -24,37 +28,44 @@ fn global_collection_getter(
     integrity_zome_name: &str,
     collection_name: &str,
     link_type_name: &str,
-) -> String {
+) -> TokenStream {
+    let get_collection_function_name =
+        format_ident!("get_{}", collection_name.to_case(Case::Snake));
+    let link_type_name = format_ident!("{link_type_name}");
+    let integrity_zome_name = format_ident!("{integrity_zome_name}");
     let snake_collection_name = collection_name.to_case(Case::Snake);
 
-    format!(
-        r#"use hdk::prelude::*;
-use {integrity_zome_name}::*;
+    quote! {
+        use hdk::prelude::*;
+        use #integrity_zome_name::*;
 
-#[hdk_extern]
-pub fn get_{snake_collection_name}() -> ExternResult<Vec<Link>> {{
-    let path = Path::from("{snake_collection_name}");
-    get_links(GetLinksInputBuilder::try_new(path.path_entry_hash()?, LinkTypes::{link_type_name})?.build())
-}}
-"#,
-    )
+        #[hdk_extern]
+        pub fn #get_collection_function_name() -> ExternResult<Vec<Link>> {
+            let path = Path::from(#snake_collection_name);
+            get_links(GetLinksInputBuilder::try_new(path.path_entry_hash()?, LinkTypes::#link_type_name)?.build())
+        }
+    }
 }
 
 fn by_author_collection_getter(
     integrity_zome_name: &str,
     collection_name: &str,
     link_type_name: &str,
-) -> String {
-    format!(
-        r#"use hdk::prelude::*;
-use {integrity_zome_name}::*;
+) -> TokenStream {
+    let get_collection_function_name =
+        format_ident!("get_{}", collection_name.to_case(Case::Snake));
+    let link_type_name = format_ident!("{link_type_name}");
+    let integrity_zome_name = format_ident!("{integrity_zome_name}");
 
-#[hdk_extern]
-pub fn get_{collection_name}(author: AgentPubKey) -> ExternResult<Vec<Link>> {{
-    get_links(GetLinksInputBuilder::try_new(author, LinkTypes::{link_type_name})?.build())
-}}
-"#,
-    )
+    quote! {
+        use hdk::prelude::*;
+        use #integrity_zome_name::*;
+
+        #[hdk_extern]
+        pub fn #get_collection_function_name(author: AgentPubKey) -> ExternResult<Vec<Link>> {
+            get_links(GetLinksInputBuilder::try_new(author, LinkTypes::#link_type_name)?.build())
+        }
+    }
 }
 
 fn add_create_link_in_create_function(
@@ -84,81 +95,74 @@ fn add_create_link_in_create_function(
 
     let snake_case_entry_type = entry_type_reference.entry_type.to_case(Case::Snake);
 
-    let mut create_link_stmts = match entry_type_reference.reference_entry_hash {
-        true => vec![format!(
-            "let {}_entry_hash = hash_entry(&{})?;",
-            snake_case_entry_type, snake_case_entry_type
-        )],
-        false => vec![],
+    let mut create_link_stmts: Vec<syn::Stmt> = if entry_type_reference.reference_entry_hash {
+        let entry_hash_variable_name = format_ident!("{snake_case_entry_type}_entry_hash");
+        let snake_case_entry_type = format_ident!("{snake_case_entry_type}");
+        vec![parse_quote! {
+            let #entry_hash_variable_name = hash_entry(&#snake_case_entry_type)?;
+        }]
+    } else {
+        vec![]
     };
 
-    let link_to_variable = match entry_type_reference.reference_entry_hash {
-        true => format!("{}_entry_hash", snake_case_entry_type),
-        false => format!("{}_hash", snake_case_entry_type),
+    let link_to_variable = if entry_type_reference.reference_entry_hash {
+        format_ident!("{snake_case_entry_type}_entry_hash")
+    } else {
+        format_ident!("{snake_case_entry_type}_hash")
     };
+    let link_type_name = format_ident!("{link_type_name}");
 
     match collection_type {
         CollectionType::Global => {
-            create_link_stmts.push(format!(r#"let path = Path::from("{}");"#, collection_name));
-            create_link_stmts.push(format!(
-                r#"create_link(path.path_entry_hash()?, {}.clone(), LinkTypes::{}, ())?;"#,
-                link_to_variable, link_type_name
-            ));
+            create_link_stmts.push(parse_quote! {let path = Path::from(#collection_name);});
+            create_link_stmts.push(parse_quote! {
+                create_link(path.path_entry_hash()?, #link_to_variable.clone(), LinkTypes::#link_type_name, ())?;
+            });
         }
         CollectionType::ByAuthor => {
-            create_link_stmts
-                .push("let my_agent_pub_key = agent_info()?.agent_latest_pubkey;".to_string());
-            create_link_stmts.push(format!(
-                r#"create_link(my_agent_pub_key, {}.clone(), LinkTypes::{}, ())?;"#,
-                link_to_variable, link_type_name
-            ));
+            create_link_stmts.push(parse_quote! {
+                let my_agent_pub_key = agent_info()?.agent_latest_pubkey;
+            });
+            create_link_stmts.push(parse_quote! {
+                create_link(my_agent_pub_key, #link_to_variable.clone(), LinkTypes::#link_type_name, ())?;
+            });
         }
     };
-
-    let stmts = create_link_stmts
-        .into_iter()
-        .map(|s| syn::parse_str::<syn::Stmt>(s.as_str()))
-        .collect::<Result<Vec<syn::Stmt>, syn::Error>>()?;
 
     let crate_src_path = zome_file_tree.zome_crate_path.join("src");
 
     let mut file_tree = zome_file_tree.dna_file_tree.file_tree();
 
-    let v: Vec<OsString> = crate_src_path
-        .clone()
-        .iter()
-        .map(|s| s.to_os_string())
-        .collect();
+    let v: Vec<OsString> = crate_src_path.iter().map(|s| s.to_os_string()).collect();
     map_rust_files(
         file_tree
             .path_mut(&mut v.iter())
             .ok_or(ScaffoldError::PathNotFound(crate_src_path.clone()))?,
-        |_file_path, mut file| {
+        |_, mut file| {
             file.items = file
                 .items
                 .into_iter()
-                .map(|i| {
-                    if let syn::Item::Fn(mut item_fn) = i.clone() {
+                .map(|item| {
+                    if let syn::Item::Fn(mut item_fn) = item.clone() {
                         if item_fn
                             .attrs
                             .iter()
-                            .any(|a| a.path().segments.iter().any(|s| s.ident.eq("hdk_extern")))
-                            && item_fn.sig.ident.eq(&fn_name.sig.ident)
+                            .any(|a| a.path().segments.iter().any(|s| s.ident == "hdk_extern"))
+                            && item_fn.sig.ident == fn_name.sig.ident
                         {
-                            for new_stmt in stmts.clone() {
+                            if let Some(return_stmt) = item_fn.block.stmts.pop() {
                                 item_fn
                                     .block
                                     .stmts
-                                    .insert(item_fn.block.stmts.len() - 1, new_stmt);
+                                    .extend(create_link_stmts.clone().into_iter());
+                                item_fn.block.stmts.push(return_stmt);
                             }
                             return syn::Item::Fn(item_fn);
                         }
                     }
-
-                    i
+                    item
                 })
                 .collect();
-
             Ok(file)
         },
     )
@@ -199,91 +203,93 @@ fn add_delete_link_in_delete_function(
     let zome_file_tree = ZomeFileTree::from_zome_manifest(dna_file_tree, chosen_coordinator_zome)?;
 
     let snake_case_entry_type = entry_type_reference.entry_type.to_case(Case::Snake);
-    let pascal_case_entry_type = entry_type_reference.entry_type.to_case(Case::Pascal);
+    let pascal_entry_def_name = entry_type_reference.entry_type.to_case(Case::Pascal);
 
-    let target_hash_variable = match entry_type_reference.reference_entry_hash {
-        true =>
-            r#"record.action().entry_hash().ok_or(wasm_error!(WasmErrorInner::Guest("Record does not have an entry".to_string())))?"#.to_string()
-        ,
-        false => format!("&original_{snake_case_entry_type}_hash"),
-    };
-    let into_hash_fn = match entry_type_reference.reference_entry_hash {
-        true => "into_entry_hash()".to_string(),
-        false => "into_action_hash()".to_string(),
+    let target_hash_variable = if entry_type_reference.reference_entry_hash {
+        quote! {
+            record
+                .action()
+                .entry_hash()
+                .ok_or(wasm_error!(WasmErrorInner::Guest("Record does not have an entry".to_string())))?
+                .clone()
+        }
+    } else {
+        let original_hash = format_ident!("original_{snake_case_entry_type}_hash");
+        quote! {#original_hash}
     };
 
-    let mut delete_link_stmts: Vec<String> = vec![];
-    match collection_type {
+    let into_hash_fn = if entry_type_reference.reference_entry_hash {
+        format_ident!("into_entry_hash")
+    } else {
+        format_ident!("into_action_hash")
+    };
+
+    let delete_link_stmts: Vec<syn::Stmt> = match collection_type {
         CollectionType::Global => {
-            delete_link_stmts.push(format!(r#"let path = Path::from("{}");"#, collection_name));
-            delete_link_stmts.push(format!(
-                r#"let links = get_links(
-                    GetLinksInputBuilder::try_new(path.path_entry_hash()?, LinkTypes::{link_type_name})?.build(),
-                )?;"#,
-            ));
-            delete_link_stmts.push(format!(
-                r#"for link in links {{
-                    if let Some(hash) = link.target.{into_hash_fn} {{
-                       if hash.eq({target_hash_variable}) {{
-                            delete_link(link.create_link_hash)?;
-                        }}
-                    }}
-                }}"#,
-            ));
+            let link_type_name = format_ident!("{link_type_name}");
+            vec![
+                parse_quote! {let path = Path::from(#collection_name);},
+                parse_quote! {
+                    let links = get_links(
+                        GetLinksInputBuilder::try_new(path.path_entry_hash()?, LinkTypes::#link_type_name)?.build(),
+                    )?;
+                },
+                parse_quote! {
+                    for link in links {
+                        if let Some(hash) = link.target.#into_hash_fn() {
+                           if hash == #target_hash_variable {
+                                delete_link(link.create_link_hash)?;
+                            }
+                        }
+                    }
+                },
+            ]
         }
         CollectionType::ByAuthor => {
-            delete_link_stmts.insert(
-                0,
-                r#"
-                let record = match details {
-                    Details::Record(details) => Ok(details.record),
-                    _ => Err(wasm_error!(WasmErrorInner::Guest(String::from(
-                        "Malformed get details response"
-                    )))),
-                }?;
-            "#
-                .to_string(),
-            );
-            delete_link_stmts.insert(0, format!(r#"
-                let details = get_details(original_{snake_case_entry_type}_hash.clone(), GetOptions::default())?
-                    .ok_or(wasm_error!(WasmErrorInner::Guest(String::from("{pascal_case_entry_type} not found"))))?;
-            "#));
-            delete_link_stmts.push(format!(
-                r#"let links = get_links(
-                    GetLinksInputBuilder::try_new(record.action().author().clone(), LinkTypes::{link_type_name})?.build()
-                )?;"#,
-            ));
-            delete_link_stmts.push(format!(
-                r#"for link in links {{
-                    if let Some(hash) = link.target.{into_hash_fn} {{
-                       if hash.eq({target_hash_variable}) {{
-                            delete_link(link.create_link_hash)?;
-                        }}
-                    }}
-                }}"#,
-            ));
+            let original_hash = format_ident!("original_{snake_case_entry_type}_hash");
+            let error_message = format!("{pascal_entry_def_name} not found");
+            let link_type_name = format_ident!("{link_type_name}");
+            vec![
+                parse_quote! {
+                    let details = get_details(#original_hash.clone(), GetOptions::default())?
+                    .ok_or(
+                        wasm_error!(WasmErrorInner::Guest(#error_message.to_string()))
+                    )?;
+                },
+                parse_quote! {
+                    let record = match details {
+                        Details::Record(details) => Ok(details.record),
+                        _ => Err(wasm_error!(WasmErrorInner::Guest("Malformed get details response".to_string()))),
+                    }?;
+                },
+                parse_quote! {
+                    let links = get_links(
+                        GetLinksInputBuilder::try_new(record.action().author().clone(), LinkTypes::#link_type_name)?.build()
+                    )?;
+                },
+                parse_quote! {
+                    for link in links {
+                        if let Some(hash) = link.target.#into_hash_fn() {
+                           if hash == #target_hash_variable {
+                                delete_link(link.create_link_hash)?;
+                            }
+                        }
+                    }
+                },
+            ]
         }
     };
-
-    let stmts = delete_link_stmts
-        .iter()
-        .map(|s| syn::parse_str::<syn::Stmt>(s))
-        .collect::<Result<Vec<syn::Stmt>, syn::Error>>()?;
 
     let crate_src_path = zome_file_tree.zome_crate_path.join("src");
 
     let mut file_tree = zome_file_tree.dna_file_tree.file_tree();
 
-    let v: Vec<OsString> = crate_src_path
-        .clone()
-        .iter()
-        .map(|s| s.to_os_string())
-        .collect();
+    let v: Vec<OsString> = crate_src_path.iter().map(|s| s.to_os_string()).collect();
     map_rust_files(
         file_tree
             .path_mut(&mut v.iter())
             .ok_or(ScaffoldError::PathNotFound(crate_src_path.clone()))?,
-        |_file_path, mut file| {
+        |_, mut file| {
             file.items = file
                 .items
                 .into_iter()
@@ -292,23 +298,22 @@ fn add_delete_link_in_delete_function(
                         if item_fn
                             .attrs
                             .iter()
-                            .any(|a| a.path().segments.iter().any(|s| s.ident.eq("hdk_extern")))
-                            && item_fn.sig.ident.eq(&fn_name.sig.ident)
+                            .any(|a| a.path().segments.iter().any(|s| s.ident == "hdk_extern"))
+                            && item_fn.sig.ident == fn_name.sig.ident
                         {
-                            for new_stmt in stmts.clone() {
+                            if let Some(delete_stmt) = item_fn.block.stmts.pop() {
                                 item_fn
                                     .block
                                     .stmts
-                                    .insert(item_fn.block.stmts.len() - 1, new_stmt);
+                                    .extend(delete_link_stmts.clone().into_iter());
+                                item_fn.block.stmts.push(delete_stmt);
                             }
                             return syn::Item::Fn(item_fn);
                         }
                     }
-
                     item
                 })
                 .collect();
-
             Ok(file)
         },
     )
@@ -366,7 +371,6 @@ pub fn add_collection_to_coordinators(
     }?;
 
     // 1. Create an INDEX_NAME.rs in "src/", with the appropriate zome functions
-
     let zome_file_tree = ZomeFileTree::from_zome_manifest(
         integrity_zome_file_tree.dna_file_tree,
         coordinator_zome.clone(),
@@ -387,18 +391,18 @@ pub fn add_collection_to_coordinators(
 
     let crate_src_path = zome_file_tree.zome_crate_path.join("src");
     let collection_path = crate_src_path.join(format!("{}.rs", snake_link_type_name.clone()));
-    insert_file(&mut file_tree, &collection_path, &getter)?;
+
+    let file = unparse_pretty(&syn::parse_quote! { #getter });
+
+    insert_file(&mut file_tree, &collection_path, &file)?;
 
     // 2. Add this file as a module in the entry point for the crate
-
     let lib_rs_path = crate_src_path.join("lib.rs");
 
-    map_file(&mut file_tree, &lib_rs_path, |s| {
+    map_file(&mut file_tree, &lib_rs_path, |contents| {
         Ok(format!(
-            r#"pub mod {};
-
-{}"#,
-            snake_link_type_name, s
+            r#"pub mod {snake_link_type_name};
+{contents}"#,
         ))
     })?;
 
