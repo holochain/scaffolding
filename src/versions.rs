6b162e9ef03956283a2a5e3e1dfb87a71e5b1c09
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-06-11 11:33:57 +0300

    chore: bump weekly (#304)
    * chore: bump weekly
    
    * chore: bump to 0.4.0-dev.7
    
    * nix flake update
    
    * chore: bump holochain playground cli

--- src/versions.rs
+++ src/versions.rs
@@ -5,7 +5,7 @@ pub const TRYORAMA_VERSION: &str = "^0.17.0-dev.0";
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.18.0-dev.1";
 
 /// npm: https://www.npmjs.com/package/@holochain/hc-spin
-pub const HC_SPIN_VERSION: &str = "^0.400.0-dev.0";
+pub const HC_SPIN_VERSION: &str = "^0.400.0-dev.1";
 
 /// npm: https://www.npmjs.com/package/@holo-host/web-sdk
 pub const WEB_SDK_VERSION: &str = "^0.6.19-prerelease";
@@ -22,4 +22,4 @@ pub const HOLOCHAIN_NIX_VERSION: &str = "weekly";
 /// source: https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server
 ///
 /// npm: https://www.npmjs.com/package/@holochain-playground/cli
-pub const HOLOCHAIN_PLAYGROUND_CLI_VERSION: &str = "^0.2.0";
+pub const HOLOCHAIN_PLAYGROUND_CLI_VERSION: &str = "^0.300.0-rc.0";
