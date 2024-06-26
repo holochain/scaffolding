f6814e72a14019df37b6a70eea69736841022014
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-06-26 13:25:44 +0300

    remove unnecessary depdencencies from vanilla template

--- run_test.sh
+++ run_test.sh
@@ -72,7 +72,7 @@ setup_and_build_hello_world() {
   cleanup_tmp hello-world
 
   cd $TEMPLATE_PATH
-  hc-scaffold --package-manager pnpm example hello-world
+  hc-scaffold --package-manager yarn example hello-world
   cd hello-world
 
   nix develop --command bash -c "
