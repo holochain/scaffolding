fd011de364f4583013968e671b8c7f0a8f2832d4
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-07-29 20:45:56 +0300

    fix URL warnings

--- src/versions.rs
+++ src/versions.rs
@@ -1,14 +1,14 @@
-/// npm: https://www.npmjs.com/package/@holochain/tryorama
+/// npm: <https://www.npmjs.com/package/@holochain/tryorama>
 pub const TRYORAMA_VERSION: &str = "^0.16.0";
 
-/// npm: https://www.npmjs.com/package/@holochain/client
+/// npm: <https://www.npmjs.com/package/@holochain/client>
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.17.0";
 
-/// npm: https://www.npmjs.com/package/@holochain/hc-spin
+/// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
 pub const HC_SPIN_VERSION: &str = "0.300.3";
 
 // TODO: update to 0.3 compatible version
-/// npm: https://www.npmjs.com/package/@holo-host/web-sdk
+/// npm: <https://www.npmjs.com/package/@holo-host/web-sdk>
 pub const WEB_SDK_VERSION: &str = "^0.6.19-prerelease";
 
 pub const HDI_VERSION: &str = holochain::HDI_VERSION;
@@ -17,10 +17,10 @@ pub const HDK_VERSION: &str = holochain::HDK_VERSION;
 
 pub const HOLOCHAIN_VERSION: &str = holochain::HOLOCHAIN_VERSION;
 
-/// https://github.com/holochain/holochain/tree/develop/versions
+/// <https://github.com/holochain/holochain/tree/develop/versions>
 pub const HOLOCHAIN_NIX_VERSION: &str = "0_3";
 
-/// source: https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server
+/// source: <https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server>
 ///
-/// npm: https://www.npmjs.com/package/@holochain-playground/cli
+/// npm: <https://www.npmjs.com/package/@holochain-playground/cli>
 pub const HOLOCHAIN_PLAYGROUND_CLI_VERSION: &str = "^0.300.0-rc.0";
