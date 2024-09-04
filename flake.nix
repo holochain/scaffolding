00227bc11c05be0716aecc20d3370dd027f182e9
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-09-04 21:27:10 +0300

    update flake.nix;

--- flake.nix
+++ flake.nix
@@ -114,10 +114,6 @@
             ]) ++ [
               self'.packages.hc-scaffold
             ];
-
-            shellHook = ''
-              export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-            '';
           };
 
           devShells.ci = pkgs.mkShell {
