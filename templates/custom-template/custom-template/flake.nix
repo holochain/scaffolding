69580b5041ac1e2ed0dd7a2b0014d8e98779e117
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-10-23 13:40:58 +0300

    fix: Refactor/custom templates (#397)
    * Fix custom template flake
    
    * Refactor custom templates

--- templates/custom-template/custom-template/flake.nix
+++ templates/custom-template/custom-template/flake.nix
@@ -4,26 +4,29 @@
   inputs = {
     holonix.url = "github:holochain/holonix?ref=main-0.3";
 
+    scaffolding.url = "github:holochain/scaffolding/holochain-0.3";
     nixpkgs.follows = "holonix/nixpkgs";
     flake-parts.follows = "holonix/flake-parts";
   };
 
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = builtins.attrNames inputs.holonix.devShells;
-    perSystem = { inputs', pkgs, ... }: {
+    perSystem = { inputs', pkgs, system, ... }: {
       formatter = pkgs.nixpkgs-fmt;
 
       devShells.default = pkgs.mkShell {
         inputsFrom = [ inputs'.holonix.devShells.default ];
 
-        packages = (with pkgs; [
-          nodejs_20
-          binaryen
-        ]);
+        packages = (with pkgs; [ nodejs_20 binaryen ]);
 
         shellHook = ''
           export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
         '';
+      };
+
+      packages.app = inputs.scaffolding.lib.wrapCustomTemplate {
+        inherit pkgs system;
+        customTemplatePath = ./template;
       };
     };
   };
