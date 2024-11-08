16d702c455472c103556eb4d6d713af05393b8cc
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-11-08 14:34:00 +0300

    fix: Fix misplaced doc comments (#411)
    * Fix misplaced doc comments
    
    * Slightly refactor link_type integrity codegen

--- src/scaffold/zome/coordinator.rs
+++ src/scaffold/zome/coordinator.rs
@@ -74,7 +74,7 @@ pub fn initial_lib_rs(dependencies: Option<&Vec<String>>) -> TokenStream {
         /// Whenever an action is committed, we emit a signal to the UI elements to reactively update them
         #[hdk_extern(infallible)]
         pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {
-            /// Don't modify this loop if you want the scaffolding tool to generate appropriate signals for your entries and links
+            /// Don't modify the for loop if you want the scaffolding tool to generate appropriate signals for your entries and links
             for action in committed_actions {
                 if let Err(err) = signal_action(action) {
                     error!("Error signaling new action: {:?}", err);
