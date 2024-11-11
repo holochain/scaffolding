c78a88d5c76fec7dc853dce04147af627cd659a5
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-11-11 14:22:56 +0300

    Add cachix nix config

--- flake.nix
+++ flake.nix
@@ -114,4 +114,16 @@
           };
         };
       };
+
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://holochain-ci.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+    ];
+  };
 }
