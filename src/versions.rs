23af57402654f58c8afc7b3b227b4eeb71f29a86
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-02-11 12:36:41 +0300

    Bump to 0.5.0-dev.17

--- src/versions.rs
+++ src/versions.rs
@@ -11,13 +11,13 @@ pub const HC_SPIN_VERSION: &str = "^0.500.0-dev.0";
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.6.0-dev.12";
+pub const HDI_VERSION: &str = "0.6.0-dev.13";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.5.0-dev.15";
+pub const HDK_VERSION: &str = "0.5.0-dev.16";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.5.0-dev.16";
+pub const HOLOCHAIN_VERSION: &str = "0.5.0-dev.17";
 
 /// source: <https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server>
 ///
