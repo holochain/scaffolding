e51aff14dc066c0c87dc8c21dd6ca6b525a011f7
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-02-13 21:46:13 +0300

    chore: update tryorama (#464)
    * Update tryorama
    
    * Bump version
    
    * Nix flake update

--- src/versions.rs
+++ src/versions.rs
@@ -1,5 +1,5 @@
 /// npm: <https://www.npmjs.com/package/@holochain/tryorama>
-pub const TRYORAMA_VERSION: &str = "^0.17.0";
+pub const TRYORAMA_VERSION: &str = "^0.17.1";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.18.0";
