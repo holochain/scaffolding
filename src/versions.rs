1cb7613c7055a7bcef93af7fe485852bd97e32ef
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-02-13 18:14:27 +0300

    Bump to dev.18

--- src/versions.rs
+++ src/versions.rs
@@ -1,5 +1,5 @@
 /// npm: <https://www.npmjs.com/package/@holochain/tryorama>
-pub const TRYORAMA_VERSION: &str = "^0.18.0-dev.3";
+pub const TRYORAMA_VERSION: &str = "^0.18.0-dev.4";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.0-dev.6";
@@ -11,13 +11,13 @@ pub const HC_SPIN_VERSION: &str = "^0.500.0-dev.0";
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.6.0-dev.13";
+pub const HDI_VERSION: &str = "0.6.0-dev.14";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.5.0-dev.16";
+pub const HDK_VERSION: &str = "0.5.0-dev.17";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.5.0-dev.17";
+pub const HOLOCHAIN_VERSION: &str = "0.5.0-dev.18";
 
 /// source: <https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server>
 ///
