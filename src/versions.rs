433856eab13dae7532a1692461da8c521a7c964d
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-03-13 17:01:26 +0300

    chore: bump to 0.4.2 (#471)
    * Bump versions
    
    * Nix flake update
    
    * Bump version

--- src/versions.rs
+++ src/versions.rs
@@ -2,19 +2,19 @@
 pub const TRYORAMA_VERSION: &str = "^0.17.1";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.18.0";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.18.1";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
-pub const HC_SPIN_VERSION: &str = "^0.400.0";
+pub const HC_SPIN_VERSION: &str = "^0.400.1";
 
 /// npm: <https://www.npmjs.com/package/@holo-host/web-sdk>
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.5.1";
+pub const HDI_VERSION: &str = "0.5.2";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.4.1";
+pub const HDK_VERSION: &str = "0.4.2";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.4.1";
+pub const HOLOCHAIN_VERSION: &str = "0.4.2";
