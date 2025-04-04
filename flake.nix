e8f8e52451945a66df96a7485c0c04ab3c42c472
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-04-04 15:02:04 +0300

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
                 inherit system;
                 overlays = [ (import rust-overlay) ];
               };
-              rustToolchain = pkgs.rust-bin.stable."1.80.0".minimal;
+              rustToolchain = pkgs.rust-bin.stable."1.83.0".minimal;
               craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
               crateInfo = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
 
