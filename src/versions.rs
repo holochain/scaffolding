501b1abc655d0a5132cbac1cb0f22628bed9149c
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-04-10 10:31:37 +0300

    chore: bump tryorama

--- src/versions.rs
+++ src/versions.rs
@@ -1,5 +1,5 @@
 /// npm: <https://www.npmjs.com/package/@holochain/tryorama>
-pub const TRYORAMA_VERSION: &str = "^0.18.0-dev.5";
+pub const TRYORAMA_VERSION: &str = "^0.18.0-rc.0";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.0-rc.0";
