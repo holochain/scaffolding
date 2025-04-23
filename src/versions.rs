1e4b3b2414fad5a3144d7bf99fd464ba7dae832c
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-04-23 11:07:46 +0300

    chore: Bump versions

--- src/versions.rs
+++ src/versions.rs
@@ -2,7 +2,7 @@
 pub const TRYORAMA_VERSION: &str = "^0.18.0-rc.1";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.0-rc.0";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.0";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
 pub const HC_SPIN_VERSION: &str = "^0.500.0-rc.1";
@@ -11,13 +11,13 @@ pub const HC_SPIN_VERSION: &str = "^0.500.0-rc.1";
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.6.0-rc.2";
+pub const HDI_VERSION: &str = "0.6.0";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.5.0-rc.2";
+pub const HDK_VERSION: &str = "0.5.0";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.5.0-rc.4";
+pub const HOLOCHAIN_VERSION: &str = "0.5.0";
 
 /// source: <https://github.com/darksoil-studio/holochain-playground/tree/main/packages/cli/server>
 ///
