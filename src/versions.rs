6549a8db32e9c2da3dfa6daf8e90e6b0a62bc95f
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-05-09 11:21:35 +0100

    chore: Update versions

--- src/versions.rs
+++ src/versions.rs
@@ -1,5 +1,5 @@
 /// npm: <https://www.npmjs.com/package/@holochain/tryorama>
-pub const TRYORAMA_VERSION: &str = "^0.18.0";
+pub const TRYORAMA_VERSION: &str = "^0.18.1";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.0";
@@ -11,10 +11,10 @@ pub const HC_SPIN_VERSION: &str = "^0.500.0";
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.6.2-rc.2";
+pub const HDI_VERSION: &str = "0.6.2";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.5.2-rc.2";
+pub const HDK_VERSION: &str = "0.5.2";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.5.2-rc.2";
+pub const HOLOCHAIN_VERSION: &str = "0.5.2";
