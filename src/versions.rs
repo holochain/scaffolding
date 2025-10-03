5896bb28ac192f5ba87164ab7aea3d9a30398347
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-10-03 12:11:55 +0100

    chore: Bump to Holochain 0.5.6

--- src/versions.rs
+++ src/versions.rs
@@ -2,7 +2,7 @@
 pub const TRYORAMA_VERSION: &str = "^0.18.3";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.2";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.19.3";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
 pub const HC_SPIN_VERSION: &str = "^0.500.3";
@@ -11,10 +11,10 @@ pub const HC_SPIN_VERSION: &str = "^0.500.3";
 pub const WEB_SDK_VERSION: &str = "^0.6.20-prerelease";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.6.5";
+pub const HDI_VERSION: &str = "0.6.6";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.5.5";
+pub const HDK_VERSION: &str = "0.5.6";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.5.5";
+pub const HOLOCHAIN_VERSION: &str = "0.5.6";
