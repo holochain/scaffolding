961c8d6b28a66a72fd98047e71a7f6594dc27c41
Matt <matt@buildyourweb.app>
2026-01-22 09:59:51 -0700

    build: bump holochain 0.6.1-rc.0

--- src/versions.rs
+++ src/versions.rs
@@ -1,17 +1,18 @@
 /// npm: <https://www.npmjs.com/package/@holochain/tryorama>
+/// Note: Tryorama is no longer maintained. This version is outdated.
 pub const TRYORAMA_VERSION: &str = "^0.19.0";
 
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.0";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.3-rc.0";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
-pub const HC_SPIN_VERSION: &str = "^0.600.0";
+pub const HC_SPIN_VERSION: &str = "^0.600.1-rc.0";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.7.0";
+pub const HDI_VERSION: &str = "0.7.1-rc.0";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.6.0";
+pub const HDK_VERSION: &str = "0.6.1-rc.0";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.6.0";
+pub const HOLOCHAIN_VERSION: &str = "0.6.1-rc.0";
