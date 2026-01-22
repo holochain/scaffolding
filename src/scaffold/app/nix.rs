344e42fe4b23d589b0d74ead895d66d2ed3027f3
Matt <matt@buildyourweb.app>
2026-01-22 09:59:51 -0700

    build: bump holochain 0.6.1-rc.0

--- src/scaffold/app/nix.rs
+++ src/scaffold/app/nix.rs
@@ -16,7 +16,7 @@ pub fn flake_nix() -> FileTree {
   description = "Flake for Holochain app development";
 
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main";
+    holonix.url = "github:holochain/holonix?ref=main-0.6";
 
     nixpkgs.follows = "holonix/nixpkgs";
     flake-parts.follows = "holonix/flake-parts";
