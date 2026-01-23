b9efebd3412bd7ca9b56c8e49106039a91ed09a0
mattyg <matt@buildyourweb.app>
2026-01-23 09:10:22 -0700

    feat!: removed support for any package manager except npm (#534) (#538)
    * feat!: removed support for any package manager except npm
    
    closes #532
    BREAKING CHANGE: remove any -p option
    
    Co-authored-by: Christian Visintin <christian.visintin@veeso.dev>

--- src/scaffold/web_app.rs
+++ src/scaffold/web_app.rs
@@ -1,12 +1,5 @@
 use build_fs_tree::{dir, file};
-use package_manager::PackageManager;
 use std::path::PathBuf;
-
-use crate::error::ScaffoldResult;
-use crate::reserved_words::check_for_reserved_keywords;
-use crate::templates::web_app::scaffold_web_app_template;
-use crate::templates::ScaffoldedTemplate;
-use crate::{error::ScaffoldError, file_tree::FileTree};
 
 use super::app::{
     cargo::workspace_cargo_toml,
@@ -14,14 +7,18 @@ use super::app::{
     manifests::{empty_happ_manifest, web_happ_manifest},
     nix::flake_nix,
 };
+use crate::error::ScaffoldResult;
+use crate::reserved_words::check_for_reserved_keywords;
+use crate::templates::web_app::scaffold_web_app_template;
+use crate::templates::ScaffoldedTemplate;
+use crate::{error::ScaffoldError, file_tree::FileTree};
 
-pub mod package_manager;
+pub mod npm;
 pub mod template_type;
 
 pub fn scaffold_web_app(
     app_name: &str,
     description: Option<&str>,
-    package_manager: PackageManager,
     skip_nix: bool,
     template_file_tree: &FileTree,
 ) -> ScaffoldResult<ScaffoldedTemplate> {
@@ -41,21 +38,11 @@ pub fn scaffold_web_app(
         app_file_tree
             .dir_content_mut()
             .ok_or(ScaffoldError::PathNotFound(PathBuf::new()))?
-            .insert("flake.nix".into(), flake_nix(&package_manager));
-    }
-
-    if package_manager == PackageManager::Pnpm {
-        app_file_tree
-            .dir_content_mut()
-            .ok_or(ScaffoldError::PathNotFound(PathBuf::new()))?
-            .insert(
-                "pnpm-workspace.yaml".into(),
-                file!("packages:\n - ui\n - tests"),
-            );
+            .insert("flake.nix".into(), flake_nix());
     }
 
     let scaffold_template_result =
-        scaffold_web_app_template(app_file_tree, template_file_tree, app_name, package_manager)?;
+        scaffold_web_app_template(app_file_tree, template_file_tree, app_name)?;
 
     Ok(scaffold_template_result)
 }
---
+++ src/scaffold/web_app/npm.rs
@@ -0,0 +1,74 @@
+use std::{ffi::OsString, path::Path};
+
+use colored::Colorize;
+use serde::Serialize;
+
+use crate::file_tree::FileTree;
+
+/// Node package manager helper.
+pub struct Npm;
+
+/// Represents sub-commands that can be executed by [`Npm`].
+#[derive(Debug, Clone, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum SubCommand {
+    /// Represents the install command.
+    Install,
+    /// Represents running a specific script.
+    Run(String),
+}
+
+impl Npm {
+    /// Checks if the specified lockfile exists in the provided file tree.
+    pub fn lockfile_exists(app_file_tree: &FileTree, path: &Path) -> bool {
+        let v = path
+            .iter()
+            .map(|s| s.to_os_string())
+            .collect::<Vec<OsString>>();
+        app_file_tree.path(&mut v.iter()).is_some()
+    }
+
+    /// Generates the command string for a given sub-command and optional workspace.
+    pub fn generate_run_command_string(sub_command: SubCommand, workspace: Option<&str>) -> String {
+        match sub_command {
+            SubCommand::Install => "npm install".to_string(),
+            SubCommand::Run(script) => match workspace {
+                Some(workspace) => format!("npm run {script} --workspace {workspace}"),
+                None => format!("npm run {script}"),
+            },
+        }
+    }
+}
+
+impl std::fmt::Display for Npm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", "npm".bright_red())
+    }
+}
+
+impl From<&str> for SubCommand {
+    fn from(s: &str) -> Self {
+        match s {
+            "install" => SubCommand::Install,
+            script => SubCommand::Run(script.to_string()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        let expected_command = "npm install";
+        let actual_command = Npm::generate_run_command_string(SubCommand::Install, None);
+        assert_eq!(expected_command, actual_command);
+
+        let expected_command = "npm run package --workspace ui";
+        let actual_command =
+            Npm::generate_run_command_string(SubCommand::Run("package".to_string()), Some("ui"));
+        assert_eq!(expected_command, actual_command);
+    }
+}
