ef4db7540a37468378d6a91fd4b45f54c2c34bd1
Matt <matt@buildyourweb.app>
2026-02-18 09:58:44 -0700

    build: bump hc-spin

--- src/versions.rs
+++ src/versions.rs
@@ -2,7 +2,7 @@
 pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.21.0-dev.2";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
-pub const HC_SPIN_VERSION: &str = "^0.700.0-dev.0";
+pub const HC_SPIN_VERSION: &str = "^0.700.0-dev.1";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
 pub const HDI_VERSION: &str = "0.8.0-dev.4";
@@ -11,4 +11,4 @@ pub const HDI_VERSION: &str = "0.8.0-dev.4";
 pub const HDK_VERSION: &str = "0.7.0-dev.6";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.7.0-dev.9";
+pub const HOLOCHAIN_VERSION: &str = "0.7.0-dev.12";
