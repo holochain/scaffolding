0e17053392615dfc7caa2d21cb526f5a2232d733
Matt <matt@buildyourweb.app>
2026-03-09 12:07:45 -0700

    fix: bump holochain, hdk, hdi, ensure test app builds again

--- src/versions.rs
+++ src/versions.rs
@@ -5,10 +5,10 @@ pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.21.0-dev.2";
 pub const HC_SPIN_VERSION: &str = "^0.700.0-dev.1";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.8.0-dev.4";
+pub const HDI_VERSION: &str = "0.8.0-dev.7";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.7.0-dev.6";
+pub const HDK_VERSION: &str = "0.7.0-dev.10";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.7.0-dev.12";
+pub const HOLOCHAIN_VERSION: &str = "0.7.0-dev.16";
