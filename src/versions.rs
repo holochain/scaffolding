07b3057eebd2335d924f8e34add69bce18628e9f
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-05-14 11:24:19 +0100

    feat: Update to Holochain 0.6.1

--- src/versions.rs
+++ src/versions.rs
@@ -1,14 +1,14 @@
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.3-rc.0";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.4";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
-pub const HC_SPIN_VERSION: &str = "^0.600.2-rc.0";
+pub const HC_SPIN_VERSION: &str = "^0.601.0";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.7.1-rc.1";
+pub const HDI_VERSION: &str = "0.7.1";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.6.1-rc.1";
+pub const HDK_VERSION: &str = "0.6.1";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.6.1-rc.1";
+pub const HOLOCHAIN_VERSION: &str = "0.6.1";
