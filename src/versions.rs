e09da5097ef0699daf5541eb13eb83a3ca3536fb
Callum Dunster <cdunster@users.noreply.github.com>
2026-07-22 09:33:13 +0200

    feat: update to Holochain 0.6.3

--- src/versions.rs
+++ src/versions.rs
@@ -1,14 +1,14 @@
 /// npm: <https://www.npmjs.com/package/@holochain/client>
-pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.7";
+pub const HOLOCHAIN_CLIENT_VERSION: &str = "^0.20.8";
 
 /// npm: <https://www.npmjs.com/package/@holochain/hc-spin>
-pub const HC_SPIN_VERSION: &str = "^0.602.0";
+pub const HC_SPIN_VERSION: &str = "^0.603.0";
 
 /// crates.io <https://crates.io/crates/hdi/versions>
-pub const HDI_VERSION: &str = "0.7.2";
+pub const HDI_VERSION: &str = "0.7.3";
 
 /// crates.io <https://crates.io/crates/hdk/versions>
-pub const HDK_VERSION: &str = "0.6.2";
+pub const HDK_VERSION: &str = "0.6.3";
 
 /// crates.io <https://crates.io/crates/holochain/versions>
-pub const HOLOCHAIN_VERSION: &str = "0.6.2";
+pub const HOLOCHAIN_VERSION: &str = "0.6.3";
